1d48cb6edd4644397941e6b791eca7028ca3c8d5
Josue Guevara <el.jdragon@gmail.com>
2025-08-26 16:03:01 -0500

    Base para el programa

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Menu principal</h1>
+    <ul>
+        <li>
+            <a href="flappy.html">Flappy Bird</a>
+        </li>
+        <li>
+            <a href="Serpiente.php">Snake</a>
+        </li>
+        <li>
+            <a href="tetrisMenu.html">Tetris</a>
+        </li>
+    </ul>
+</body>
+</html>
