3b3407b82ee852c2fbb583fb9328cb148e27ad7c
Josue Guevara <el.jdragon@gmail.com>
2025-08-26 16:22:49 -0500

    Cambio del menu

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Menu principal</h1>
-    <ul>
+    <h1>Grupo[]</h1>
+    <nav>
+        <ul>
         <li>
             <a href="flappy.html">Flappy Bird</a>
         </li>
@@ -18,5 +20,6 @@
             <a href="tetrisMenu.html">Tetris</a>
         </li>
     </ul>
+    
 </body>
 </html>
